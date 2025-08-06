34cca28f3fb77791c884af04e9627be2cf702646
ram-nimse <nimseram77@gmail.com>
2025-08-06 16:55:36 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'table-tracking-customer',
+  webDir: 'www'
+};
+
+export default config;
